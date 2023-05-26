d615666382d2da500733188dad9ed6b3a97d8127
Muhammad Hamza <muhammadhamza1311@gmail.com>
2023-05-26 17:29:56 +0500

    Use gloo-utils for converting to JsValue (#8)

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,5 +1,6 @@
 use wasm_bindgen::prelude::*;
 use json_patch::PatchOperation;
+use gloo_utils::format::JsValueSerdeExt;
 
 #[wasm_bindgen(js_name = createPatch)]
 pub fn create_patch(left: JsValue, right: JsValue) -> Result<String, serde_wasm_bindgen::Error> {
@@ -14,5 +15,5 @@ pub fn apply_patch(doc: JsValue, patches: JsValue) -> Result<JsValue, serde_wasm
     let mut doc = serde_wasm_bindgen::from_value(doc)?;
     let patches: Vec<PatchOperation> = serde_wasm_bindgen::from_value(patches)?;
     json_patch::patch(&mut doc, &patches).expect("todo");
-    serde_wasm_bindgen::to_value(&doc)
+    JsValue::from_serde(&doc).map_err(|e| serde_wasm_bindgen::Error::new(&e.to_string()))
 }
