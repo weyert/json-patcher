168834940d63b82603f4e82af40c5fcb5c815c03
Muhammad Hamza <muhammadhamza1311@gmail.com>
2023-08-31 19:13:47 +0500

    tests for dates in arrays

--- test/dates.spec.ts
+++ test/dates.spec.ts
@@ -7,14 +7,26 @@ it(`dates round trip`, () => {
         date: new Date(2023, 6, 9),
         systemHeader: {
             date: new Date(2023, 4, 20)
-        }
-    }
+        },
+        arr: [
+            new Date(2023, 4, 20),
+            {
+                date: new Date(2023, 4, 20)
+            },
+        ]
+    } as const
     const output = {
         date: new Date(2023, 4, 20),
         systemHeader: {
             date: new Date(2023, 6, 9)
-        }
-    }
+        },
+        arr: [
+            new Date(2023, 6, 9),
+            {
+                date: new Date(2023, 6, 9)
+            }
+        ]
+    } as const
     const patch = createPatch(input, output)
     const actualOutput = applyPatch(input, patch)
 
@@ -24,6 +36,9 @@ it(`dates round trip`, () => {
     // this may have something to do with jest, as in neon_serde tests with mocha, the assertion is successful
     expect(actualOutput.date.toISOString()).to.equal(output.date.toISOString())
     expect(actualOutput.systemHeader.date.toISOString()).to.equal(output.systemHeader.date.toISOString())
+    expect(actualOutput.arr[0].toISOString()).to.equal(output.arr[0].toISOString())
+    expect(actualOutput.arr[1].date.toISOString()).to.equal(output.arr[1].date.toISOString())
+
 })
 
 it(`patch has special date string`, () => {
